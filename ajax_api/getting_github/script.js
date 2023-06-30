edfe3665cc1df325df9fa0a481908e5f1250d000
BThomas <bran_thom@yahoo.com>
2023-06-30 14:56:27 -0500

    deleted line

--- ajax_api/getting_github/script.js
+++ ajax_api/getting_github/script.js
@@ -7,10 +7,9 @@ async function apicall()
     var coderData = await response.json();
     var count = coderData.followers
     var element = document.querySelector("#propic")
-    console.log(count)
+    
     var h1 = document.querySelector("h1");
     element.src = coderData.avatar_url
-    document.getElementById("bntID")
     
     h1.innerText = "Follower count: " + count;
 
